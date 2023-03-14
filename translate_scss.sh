e6cf2002caa2c7d84c45ee569a5a81c389d3f576
olegskip <skip.cpp.reg@gmail.com>
2023-03-14 12:06:16 +0200

    Final refactoring

--- translate_scss.sh
+++ translate_scss.sh
@@ -1,8 +1,8 @@
 mkdir -p styles
 for f in `fd . ./scss -e scss`; do
-    # remove extension
-    file_name=${f%.*}
-    # remove path
-    file_name=${file_name##*/}
-    sass $f ./styles/$file_name.css
+	# remove extension
+	file_name=${f%.*}
+	# remove path
+	file_name=${file_name##*/}
+	sass $f ./styles/$file_name.css
 done
